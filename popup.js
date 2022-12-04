1c37d6a10eca87e305f7e974f4b05b3389fe6b2e
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-12-04 11:28:40 +0100

    feat: finish up extension

--- popup.js
+++ popup.js
@@ -1,23 +1,40 @@
-function doomsDay() {
-  const colorPalette = document.querySelector(".color");
+const redButton = document.querySelector("#redButton")
 
-  //   for (let i = 0; i < colorPalette.length; i++) {
-  //     const element = colorPalette[i];
-  colorPalette.addEventListener("click", () => {
-    document.body.style.backgroundColor =
-      window.getComputedStyle(colorPalette).backgroundColor;
-  });
-  //   }
+const colors = document.querySelectorAll(".color")
+
+const colorPicker = document.querySelector("#colorPicker")
+
+const changeBackgroundColor = (bgColor = "red") => {
+	const allElementsOnPage = document.querySelectorAll("*")
+
+	allElementsOnPage.forEach(element => {
+		element.style.backgroundColor = bgColor
+	})
 }
 
-(async function nukeIt() {
-  let [tab] = await chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true,
-  });
+colorPicker.addEventListener("change", async e => {
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
 
-  await chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: doomsDay,
-  });
-})();
+	await chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+
+		func: changeBackgroundColor,
+		args: [e.target.value],
+	})
+})
+
+colors.forEach(colorButton => {
+	let theColor = colorButton.getAttribute("color")
+
+	colorButton.addEventListener("click", async () => {
+		alert(theColor)
+		let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+
+		await chrome.scripting.executeScript({
+			target: { tabId: tab.id },
+
+			func: changeBackgroundColor,
+			args: [theColor],
+		})
+	})
+})
